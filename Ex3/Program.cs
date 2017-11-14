4187a69ca572c29f0aafe81517e92c2abc09227a
jesuslermatvr <jesus.lermatvr@gmail.com>
2017-11-14 17:59:06 -0600

    Ex3 version patito 6.0

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -25,7 +25,11 @@ namespace Ex3
         public static double triangleArea(double a, double b, double c) {
             
             double salida;
-            salida=(a+b+c)/2;
+            if( a>0 & b>0 & c>0)
+            {
+                 salida=(a+b+c)/2;
+            }
+        
             return salida;
         }
     }
