b85fb74ebab3540be38a5645e3d4c367b8b15e6f
Roberto Meraz <robrtmty@gmail.com>
2017-11-14 17:43:24 -0600

    Ex5 ya se puede 1.0

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -21,9 +21,30 @@ namespace Ex5
         }
 
         public static string SupressWhiteSpaces(string text) {          
-            string Suprimir=$"{text.Trim()}";
-            if(string.IsNullOrEmpty(text)){
+            string Suprimir="";
+            int i=0;
+            int j=0;
+            int borrar[15]=0;
+            int mantener[15]=0;
+             if(string.IsNullOrEmpty(text)){
                 Suprimir="";
+            }
+            else{
+                int lng=text.Lenght();
+                for(i=0;i<lng;i++){
+                    if(text[i]==" "){
+                        borrar[i]=i;
+                    }
+                    else{
+                        mantener[i]=i;
+                    }
+                }
+            }
+
+            for(j=0;j<lng;j++){
+                if(mantener[j]!=0){
+                    Suprimir+=mantener[j];
+                }
             }
             return Suprimir;
         }
