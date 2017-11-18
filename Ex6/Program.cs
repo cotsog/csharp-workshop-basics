4645550c91c59690fd8fee825c5f8ae9c1358939
Roberto Meraz <robrtmty@gmail.com>
2017-11-18 10:21:15 -0600

    Ex6 2.0

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -19,7 +19,7 @@ namespace Ex6
 {
     public class Program
     {
-        public static bool ENABLED = false;
+        public static bool ENABLED = true;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
