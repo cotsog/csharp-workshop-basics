65af083290483e25fa2457f88616945452b5ee46
brandonlfime <brandonlbmx98@gmail.com>
2017-11-18 09:46:39 -0600

    v1

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -14,7 +14,7 @@ namespace Ex3
 {
     public class Program
     {
-        public static bool ENABLED = false;
+        public static bool ENABLED = true;
 
         static void Main(string[] args)
         {
@@ -22,7 +22,11 @@ namespace Ex3
         }
 
         public static double triangleArea(double a, double b, double c) {
-            return 0.0;
+
+        if(a <= 0 || b <= 0 || c <= 0)
+        return -1;
+            triangleArea = (a + b + c)/ 2;
+            return triangleArea;
         }
     }
 }
